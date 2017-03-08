5429140ec98aa2b1011a826ca457b9f09b762471
tylerco <tyler.cooper@outlook.com>
2017-03-07 20:53:33 -0500

    First Release
    This is a very early version of the script, my intention is to set it up
    so that it checks the temperature every 5 minutes and displays a new
    notifcation.

---
+++ fb-toast.ps1
@@ -0,0 +1,56 @@
+﻿##FlameBoss Toast Notification Command
+## This script is very much a work in progress
+## To execute run fb-toast -DoneMeatTemp '195 -RawURL "https://myflameboss.com/cooks/#####/raw"
+
+## Notification code was created by:https://gist.github.com/altrive/72594b8427b2fff16431
+
+Param(
+  [string]$DoneMeatTemp,
+  [string]$RawURL
+)
+
+function tempcheck {
+param ($DoneMeatTemp, $RawURL)
+## Get Data
+$TempC = Invoke-WebRequest $RawURL | ConvertFrom-Csv | select meat_temp1,pit_temp -Last 1
+
+$TestVar = $TempC.meat_temp1
+$TempF =  ((32+($TestVar/10)*1.8))
+$PitTempF = ((32+($TempC.pit_temp/10)*1.8))
+
+$PCTCooked = $TempF / $DoneMeatTemp
+
+## Code Block for Notification
+$ErrorActionPreference = "Stop"
+
+#$notificationTitle = "Notification: " + [DateTime]::Now.ToShortTimeString()
+$notificationTitle = "Current Temp: " + $TempF + " Pit Temp: " + $PitTempF + " Completion: " + $PCTCooked
+
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
+
+#Convert to .NET type for XML manipuration
+$toastXml = [xml] $template.GetXml()
+$toastXml.GetElementsByTagName("text").AppendChild($toastXml.CreateTextNode($notificationTitle)) > $null
+
+#Convert back to WinRT type
+$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+$xml.LoadXml($toastXml.OuterXml)
+
+$toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
+$toast.Tag = "PowerShell"
+$toast.Group = "PowerShell"
+$toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(5)
+#$toast.SuppressPopup = $true
+
+If ($tempF -ne $DoneMeatTemp) {$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell");
+$notifier.Show($toast);} 
+
+#$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+#$notifier.Show($toast);
+## End Notify
+
+}
+
+tempcheck -DoneMeatTemp $DoneMeatTemp -RawURL $RawURL
+
